f29289c50fa81ad6f74c0361931ae3090f837f98
unknown <studajay3@gmail.com>
2023-08-27 15:09:48 +0530

    added another method to do quick sort

--- sorting/quicksort.py
+++ sorting/quicksort.py
@@ -11,3 +11,28 @@ def quicksort(arr):
 arr = [25,75,1,31,9,2,7,7]
 sorted_arr = quicksort(arr)
 print("Sorted array:", sorted_arr)
+
+
+# another method
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[0]
+    left = []
+    right = []
+
+    for num in arr[1:]:
+        if num < pivot:
+            left.append(num)
+        else:
+            right.append(num)
+
+    return quicksort(left) + [pivot] + quicksort(right)
+
+
+# Example usage
+input_array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+sorted_array = quicksort(input_array)
+print(sorted_array)
