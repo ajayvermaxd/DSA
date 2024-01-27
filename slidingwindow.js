80f6ce7202c17d94010bd326870be086c9bc48d9
ajayvermaxd <studajay3@gmail.com>
2024-01-27 17:14:48 +0530

    sliding window algo

--- slidingwindow.js
+++ slidingwindow.js
@@ -23,3 +23,11 @@ function longestsubstring(s) {
 let input = "abcabcdbb";
 let result = longestsubstring(input);
 console.log(result);
+
+//  question: There is a drill in the army. The Army general wants to know the best cumulative power of his troops.
+// Troops give the best results when the maximum different level of a soldier stands together. If a troop
+// contains 2 same levels of a soldier their power becomes 0. Let assume there are x different levels in the
+// army, You have to find the power of the best troop from the army given. Assume 1 soldier power to be 1
+// unit.
+// 1) Input: army = "wpwkewe" (w,p,k,e : are different levels of soldier) Output: 4 At max 4 is max power of
+// any troop formed. (pwke)
